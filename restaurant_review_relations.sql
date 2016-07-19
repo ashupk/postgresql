7a7d906558a03c130bcf44f8307097327ae16eb4
Matthew <mbrimmer1@gmail.com>
2016-07-19 17:04:29 -0400

    More Exercises

--- restaurant_review_relations.sql
+++ restaurant_review_relations.sql
@@ -12,3 +12,83 @@ where
   users.id = reviews.author_user_id and
   restaurants.id = reviews.restaurant_id
 ;
+--average stars by restaurant
+select
+  users.id,
+  users.name,
+  avg(reviews.stars) as average_stars
+from
+ users,
+ restaurants,
+ reviews
+where
+  users.id = reviews.author_user_id and
+  restaurants.id = reviews.restaurant_id
+group by
+  users.id
+;
+
+--average stars by user
+select
+  restaurants.id,
+  restaurants.name,
+  avg(reviews.stars) as average_stars
+from
+ users,
+ restaurants,
+ reviews
+where
+  users.id = reviews.author_user_id and
+  restaurants.id = reviews.restaurant_id
+group by
+  restaurants.id
+;
+
+--Lowest star rating by user
+select
+  users.id,
+  users.name,
+  min(reviews.stars) as lowest_stars_rating
+from
+ users,
+ restaurants,
+ reviews
+where
+  users.id = reviews.author_user_id and
+  restaurants.id = reviews.restaurant_id
+group by
+  users.id
+;
+
+--Count number of restaurant reviews
+select
+  restaurants.id,
+  restaurants.name,
+  count(reviews.restaurant_id) as number_of_reviews
+from
+ users,
+ restaurants,
+ reviews
+where
+  users.id = reviews.author_user_id and
+  restaurants.id = reviews.restaurant_id
+group by
+  restaurants.id
+;
+
+--Number of restaurants in each category and count
+select
+  restaurants.id,
+  restaurants.name,
+  restaurants.category,
+  count(restaurants.name) as category
+from
+ users,
+ restaurants,
+ reviews
+where
+  users.id = reviews.author_user_id and
+  restaurants.id = reviews.restaurant_id
+group by
+  reviews.id, restaurants.name, restaurants.id restaurants.category
+;
