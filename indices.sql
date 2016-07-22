39c0ae641b0752e96eb70ed7d0615bb4b36340af
Matthew <mbrimmer1@gmail.com>
2016-07-22 17:37:08 -0400

    More DB Exercises

---
+++ indices.sql
@@ -0,0 +1,68 @@
+-- When to index
+
+
+
+
+
+-- Situation 1: looking for specific value
+
+select
+  *
+from
+  restaurant
+where
+  name = 'NaanStop';
+
+-- solution:
+
+CREATE INDEX on restaurant (name);
+
+
+
+
+
+
+
+
+
+
+-- Situation 2: sorting
+
+select
+  *
+from
+  reviewer
+order by
+  stars desc
+limit
+  5;
+
+-- Reduce O(n * log n) time sort to O(log n):
+
+CREATE INDEX on reviewer (stars);
+
+
+
+
+
+
+
+
+
+-- Situation 3: join
+
+select
+	tweet.content
+from
+	author
+inner join
+	tweet on author.id = tweet.author_id
+where
+	author.name = 'phamous2day';
+
+
+
+
+
+
+-- FIN
